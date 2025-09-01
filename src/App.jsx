89e355aadf1931fa6bf9628d2d2d2b1d388364e7
ChaitanyaV98 <chaitanya@betalectic.com>
2025-09-01 10:12:07 +0530

    setup vite, react and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-800">
+        Tailwind CSS v4.1 + React + Vite
+      </h1>
+    </div>
+  );
+}
+
+export default App;
